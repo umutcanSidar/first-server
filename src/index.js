0d134e79e06a7119f946a7ad7a98c6e27bf6b7b7
Umutcan <umutcansidar@gmail.com>
2022-06-21 23:24:31 +0300

    set header

--- src/index.js
+++ src/index.js
@@ -11,6 +11,25 @@ const port = process.env.PORT || 3000;
 
 const uri = process.env.URI;
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // handle OPTIONS method
+  if ('OPTIONS' == req.method) {
+    return res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
